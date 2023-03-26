c5e0eaac3e07e0d9622342a397ecdb203b4cb44a
PedroVictor022 <pedro20002victor@gmail.com>
2023-03-25 23:05:27 -0300

    up

---
+++ src/components/header/index.tsx
@@ -0,0 +1,14 @@
+function Header() {
+  return (
+    <div className="flex content-center justify-between p-2 bg-zinc-900 text-white">
+      <div>
+        <h1 className="text-4xl">BRASTOCK</h1>
+      </div>
+      <div>
+        <p>ICON</p>
+        <p>MENU</p>
+      </div>
+    </div>
+  )
+}
+export default Header;
